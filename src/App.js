e2b44c1b9c5d7c8863512b1820141613905c873f
Mahmoud Abada <nm_abada@esi.dz>
2024-12-04 11:23:56 -0600

    initialzing project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Workshops from './components/Workshops';
+
+import Agenda from './components/Agenda';
+import Sponsors from './components/Sponsors';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Sponsors/>
+      <Workshops/>
+      <Contact/>
+      <Agenda/>
+
+     
+     
+    </div>
+  );
+}
+
+export default App;
